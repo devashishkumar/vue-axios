968955537bf31afbf9de17ffca90501c5a4733e0
arora.ashish0512 <arora.ashish0512@gmail.com>
2021-03-07 12:35:10 +0530

    axios setup

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,27 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.headers = {
+  'Authorization': 'Bearer test token',
+  'Content-Type': 'application/json'
+};
+axios.defaults.timeout = 2000;
+axios.defaults.withCredentials = false;
+// axios.defaults.proxy = {
+//   protocol: 'https',
+//   host: '127.0.0.0',
+//   port: 1203,
+//   auth: {
+//     username: 'test',
+//     password: 'test'
+//   }
+// };
+
+
+
 createApp(App)
   .use(router)
   .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,30 @@
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// const DATA_URL = "https://api.covid19india.org/state_district_wise.json";
+import axios from 'axios';
+const DATA_URL = 'http://localhost:3000/users';
 
 @Options({
   components: {
     HelloWorld
-  }
+  }, created() {
+    // fetch(DATA_URL)
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     window.console.log(data, '22');
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    axios.get('/users').then((response) => {
+      window.console.log(response)
+    }, (error) => {
+      window.console.log(error);
+    });
+  },
 })
 export default class Home extends Vue {}
 </script>
